f9d40d7cccbd26d22267b093ee79ebde5f2599fb
xac <jiangfengxi.c@gmail.com>
2023-11-16 15:15:57 +0800

    [feat]:b allow cors request

--- xory-back/api/src/lib.rs
+++ xory-back/api/src/lib.rs
@@ -1,8 +1,18 @@
-use axum::{routing::MethodRouter, Router};
+use axum::{
+    http::{
+        header::{ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN},
+        HeaderValue, Method,
+    },
+    routing::MethodRouter,
+    Router,
+};
 use common::db_conn;
 use sea_orm::DatabaseConnection;
 use std::env;
 mod routes;
+// use http::HeaderValue;
+use tower_http::cors::{any, CorsLayer};
+use tower_http::set_header::SetResponseHeaderLayer;
 
 #[tokio::main]
 pub async fn run() {
@@ -11,7 +21,15 @@ pub async fn run() {
         db: db_conn::get_db_conn().await,
     };
 
-    let app = Router::new().nest("/", routes::compose()).with_state(state);
+    let cors = CorsLayer::new()
+        .allow_methods(vec![Method::GET, Method::POST])
+        .allow_origin(any())
+        .allow_headers(any());
+
+    let app = Router::new()
+        .nest("/", routes::compose())
+        .layer(cors)
+        .with_state(state);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&format!("0.0.0.0:{port}").parse().unwrap())
