95972407b1be28b523496f425334e85bd9892d81
xac <jiangfengxi.c@gmail.com>
2023-11-17 14:18:20 +0800

    [feat]:f import vuetify package

--- xory-front/src/main.ts
+++ xory-front/src/main.ts
@@ -4,15 +4,21 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from '@/App.vue'
-import { router } from '@/router'
-import { i18n } from '@/i18n'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(createPinia())
+
+import { router } from '@/router'
 app.use(router())
 
+import { i18n } from '@/i18n'
 app.use(i18n())
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+const vuetify = createVuetify({})
+app.use(vuetify)
 
 app.mount('#app')
--- xory-front/vite.config.ts
+++ xory-front/vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +17,8 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    vuetify({ autoImport: true }) // Enabled by default
   ],
   resolve: {
     alias: {
