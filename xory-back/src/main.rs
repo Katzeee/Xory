87c9a897331595b958dacaa5ee835b72364654c4
xac <jiangfengxi.c@gmail.com>
2023-11-17 10:32:02 +0800

    [feat]:b pretty log

--- xory-back/src/main.rs
+++ xory-back/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     dotenvy::dotenv().expect(".env file read fail.");
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
+        .pretty()
         .init();
     api::run();
 }
